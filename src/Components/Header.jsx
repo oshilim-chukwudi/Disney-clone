f34061b9a1f37cfbcad5692e17efea5586221cd9
oshilim-chukwudi <chukwudioshilim@gmail.com>
2024-01-11 22:32:57 -0700

    Images added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Logo from "./../Components/Images/Logo.png";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <img src={Logo} className="w-[80px] object-cover" />
+    </div>
+  );
 };
